75292b1c2222bab453948b986bf2970c167e6e9a
Amy Baker <amyraebaker@gmail.com>
2022-06-04 13:19:30 -0700

    Create function to play game and return winner

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ function computerPlay() {
     return computerSelection
 }
 
+function play(playerSelection, computerSelection) {
+    if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'Scissors') || (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Rock') || (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Paper')) {
+        console.log(`You Win! ${playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()} beats ${computerSelection}`)
+        return 'player'
+    } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') || (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors') || (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock')){
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()}`)
+        return 'computer'
+    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        console.log(`You tie! You both threw ${computerSelection}`)
+        return 'tie'
+    } else {
+        return `You must select Rock, Paper, or Scissors`
+    }
+}
+
