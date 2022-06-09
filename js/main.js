435b421462e2a779a7da81833b79f5b88185e7f1
Amy Baker <amyraebaker@gmail.com>
2022-06-08 17:04:49 -0700

    Add buttons to user interface and update logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 function computerPlay() {
     let num = Math.floor(Math.random() * 3)
-    let computerSelection = num === 0 ? 'Rock' : num === 1 ? 'Paper' : 'Scissors'
+    let computerSelection = num === 0 ? 'rock' : num === 1 ? 'paper' : 'scissors'
     return computerSelection
 }
 
-function play(playerSelection, computerSelection) {
-    if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'Scissors') || (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Rock') || (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Paper')) {
+function playRound(e, computerSelection) {
+    const playerSelection = e.srcElement.id
+    if ((playerSelection === 'rock' && computerSelection === 'Scissors') || (playerSelection === 'paper' && computerSelection === 'Rock') || (playerSelection === 'scissors' && computerSelection === 'Paper')) {
         console.log(`You Win! ${playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()} beats ${computerSelection}`)
         return 'player'
-    } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') || (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors') || (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock')){
+    } else if ((playerSelection === 'rock' && computerSelection === 'Paper') || (playerSelection === 'paper' && computerSelection === 'Scissors') || (playerSelection === 'scissors' && computerSelection === 'Rock')){
         console.log(`You Lose! ${computerSelection} beats ${playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()}`)
         return 'computer'
-    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    } else if (playerSelection === computerSelection) {
         console.log(`You tie! You both threw ${computerSelection}`)
         return 'tie'
     } else {
@@ -19,18 +20,21 @@ function play(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0, computerScore = 0
-    for (let i = 0; i < 5; i++) {
-        let result = play(prompt('Make a selection'), computerPlay())
-        if (result === 'player') playerScore++
-        else if (result === 'computer') computerScore++
-        else if (result === 'tie') i--
-    }
-    let winnerMessage = playerScore > computerScore ? `You win!` : `The Computer Wins!`
-    console.log(`Final Score:
-    You : ${playerScore} Computer : ${computerScore}
-    ${winnerMessage}`)
-}
+// function game() {
+//     let playerScore = 0, computerScore = 0
+//     for (let i = 0; i < 5; i++) {
+//         let result = playRound(prompt('Make a selection'), computerPlay())
+//         if (result === 'player') playerScore++
+//         else if (result === 'computer') computerScore++
+//         else if (result === 'tie') i--
+//     }
+//     let winnerMessage = playerScore > computerScore ? `You win!` : `The Computer Wins!`
+//     console.log(`Final Score:
+//     You : ${playerScore} Computer : ${computerScore}
+//     ${winnerMessage}`)
+// }
 
-game()
+// game()
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach(button => button.addEventListener('click',playRound))
